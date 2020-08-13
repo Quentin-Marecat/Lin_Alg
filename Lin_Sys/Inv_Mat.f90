b958fb94672c67fcf4c42af48577002ddcf1a16c
Quentin <quentin.marecat@gmail.com>
2020-08-13 11:17:44 +0200

    Linear syst solving adding + Matrix inversion correction

---
+++ Lin_Sys/Inv_Mat.f90
@@ -0,0 +1,39 @@
+SUBROUTINE INV_MAT(DIM,MATRIX,VEC,SOL)
+    ! ----------------------------------------------------------------------------------------------- !
+    ! --- THIS SUBROUTINE FIND THE SOLUTION OF THE SYSTEM MATRIX*SOL = VEC USING THE INVERSE OF A --- !
+    ! --- MATRIX MUST BE SQUARE AND INVERSIBLE ------------------------------------------------------ !
+    ! --- THIS SOLUTION IS NOT THE BEST ONE BECAUSE IT INVOLVE SOME USELESS OPERATION --------------- !
+    ! ----------------------------------------------------------------------------------------------- !
+    IMPLICIT NONE 
+    REAL*8, PARAMETER :: EPS0 = 1.D-14
+    INTEGER, INTENT(IN) :: DIM
+    REAL*8, INTENT(IN) :: MATRIX(DIM,DIM),VEC(DIM)
+    REAL*8, INTENT(OUT) :: SOL(DIM)
+    REAL*8 :: INVMATRIX(DIM,DIM), VECTEST(DIM)
+    LOGICAL :: TEST
+    INTEGER :: I,ERR, STAT
+
+    OPEN(UNIT = ERR, FILE = 'error', IOSTAT = STAT, STATUS = 'old')
+    IF (STAT == 0) CLOSE(ERR,STATUS = 'delete')
+
+    CALL INVMATQR(DIM,MATRIX,INVMATRIX)
+    CALL MATAPPLI(DIM,INVMATRIX,VEC,SOL)
+
+    ! --- VERIFICATION --- !
+    CALL MATAPPLI(DIM,MATRIX,SOL,VECTEST)
+    TEST = .FALSE.
+    DO I = 1,DIM
+        IF (ABS(VECTEST(I) - VEC(I)) > EPS0) TEST = .TRUE.
+    ENDDO
+    IF (TEST) THEN
+        OPEN(UNIT = ERR,FILE = 'error')
+        WRITE(ERR,'(A)') 'PROBLEM SOLUTION'
+        WRITE(ERR,'(A)') 'x = '
+        WRITE(ERR,'(100F14.5)') (SOL(I), I = 1,DIM)
+        WRITE(ERR,'(A)') '************'
+    ENDIF
+
+    OPEN(UNIT = ERR, FILE = 'error', IOSTAT = STAT, STATUS = 'old')
+    IF (STAT == 0) WRITE(6,'(A)') 'ERROR, SEE FILE error'
+
+END SUBROUTINE
---
+++ Lin_Sys/test.f90
@@ -0,0 +1,21 @@
+PROGRAM TEST
+    IMPLICIT NONE
+    REAL*8,ALLOCATABLE :: M(:,:), VEC(:), SOL(:)
+    CHARACTER :: TEXT
+    INTEGER :: DIM,I,J
+    OPEN(UNIT = 99,FILE = 'input',STATUS = 'OLD', ACTION= 'READ')
+    OPEN(UNIT = 98,FILE = 'output')
+    READ(99,*) DIM
+    READ(99,'(A)') TEXT
+    ALLOCATE(M(DIM,DIM),VEC(DIM),SOL(DIM))
+    DO I = 1,DIM
+        READ(99,*) (M(I,J), J = 1,DIM)
+    ENDDO
+    READ(99,'(A)') TEXT
+    READ(99,*) (VEC(I),I=1,DIM)
+    CALL INV_MAT(DIM,M,VEC,SOL)
+
+    WRITE(98,'(A)') 'SOLUTION ='
+    WRITE(98,'(100F14.5)') (SOL(I),I = 1,DIM)
+    WRITE(98,*) '*******************'
+END
