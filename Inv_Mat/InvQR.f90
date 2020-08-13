900d65b728716a22d0d7628add0a34ecd4424451
Quentin <quentin.marecat@gmail.com>
2020-08-13 13:44:26 +0200

    InvQR simplification

--- Inv_Mat/InvQR.f90
+++ Inv_Mat/InvQR.f90
@@ -1,32 +1,36 @@
-SUBROUTINE INVMATQR(DIME,MATRIX,INVMATRIX)
-    USE INVMATMOD
-    ! ----------------------------------------------------------------------------- !
-    ! --- THIS SUBROUTINE INVERSE THE MATRIX USING QR HOUSEHOLDER FACTORIZATION --- !
-    ! ----------------------------------------------------------------------------- !
+SUBROUTINE INVMATQR(DIM,MATRIX,INVMATRIX)
+    ! --------------------------------------------------------------------------------------- !
+    ! --- THIS SUBROUTINE INVERSE THE MATRIX USING QR HOUSEHOLDER FACTORIZATION ------------- !
+    ! --- housestep.f90 AND QRhouse.f90 ARE USEFULL FOR THIS ALGORTIHME AND MUS BE COMPIL --- !
+    ! --- THEY CAN BE FOUND ON Quentin-Marecat GITHUB PAGE ---------------------------------- !
+    ! --------------------------------------------------------------------------------------- !
     IMPLICIT NONE
-    INTEGER :: DIME
-    REAL*8,INTENT(IN) :: MATRIX(DIME,DIME)
-    REAL*8,INTENT(OUT) :: INVMATRIX(DIME,DIME)
-    REAL*8 :: R(DIME,DIME), Q(DIME,DIME),RINV(DIME,DIME)
-    REAL*8 :: QT(DIME,DIME), ID(DIME,DIME),ID2(DIME,DIME)
-    INTEGER :: STAT
-    DIM = DIME
+    INTEGER :: DIM
+    LOGICAL :: TEST
+    REAL*8, PARAMETER :: EPS0 = 1.D-14
+    REAL*8,INTENT(IN) :: MATRIX(DIM,DIM)
+    REAL*8,INTENT(OUT) :: INVMATRIX(DIM,DIM)
+    REAL*8 :: R(DIM,DIM), Q(DIM,DIM),RINV(DIM,DIM)
+    REAL*8 :: QT(DIM,DIM), ID(DIM,DIM),ID2(DIM,DIM)
+    INTEGER :: I,J,K,ERR,STAT
+
     ID = 0.
     DO I = 1,DIM
         ID(I,I) = 1
     ENDDO
 
+    ERR = 97
     OPEN(UNIT = ERR, FILE = 'error', IOSTAT = STAT, STATUS = 'old')
     IF (STAT == 0) CLOSE(ERR,STATUS = 'delete')
     
 
-    CALL HOUSEHOLDER(MATRIX,R,Q)
+    CALL QRHOUSE(DIM,MATRIX,Q,R)
 
-    TESTLOG = .FALSE.
+    TEST = .FALSE.
     DO I = 1,DIM
-        IF(ABS(R(I,I)) < EPS0) TESTLOG = .TRUE.
+        IF(ABS(R(I,I)) < EPS0) TEST = .TRUE.
     ENDDO
-    IF (TESTLOG) THEN
+    IF (TEST) THEN
         OPEN(UNIT = ERR,FILE = 'error')
         WRITE(ERR,'(A)') 'NON INVERSIBLE MATRIX'
         WRITE(ERR,'(A)') 'R-MATRIX :'
@@ -56,13 +60,13 @@ SUBROUTINE INVMATQR(DIME,MATRIX,INVMATRIX)
     ENDIF
 
     ! --- VERIFICATION --- !
-    TESTLOG = .FALSE.
+    TEST = .FALSE.
     DO I = 1,DIM
         DO J = 1,DIM
-            IF (ABS(ID2(I,J)-ID(I,J)) > EPS0) TESTLOG = .TRUE.
+            IF (ABS(ID2(I,J)-ID(I,J)) > EPS0) TEST = .TRUE.
         ENDDO
     ENDDO
-    IF (TESTLOG) THEN
+    IF (TEST) THEN
         OPEN(UNIT = ERR,FILE = 'error')
         WRITE(ERR,'(A)') 'PROBLEM MATRIX INVERSION'
         WRITE(ERR,'(A)') 'R = '
