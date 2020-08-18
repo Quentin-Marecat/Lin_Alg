5dfc6e5195248a2d594cc4b84c8efafd564b2086
Quentin <quentin.marecat@gmail.com>
2020-08-18 18:40:12 +0200

    QR hessenberg

---
+++ QR_dec/QRhouse_hess.f90
@@ -0,0 +1,87 @@
+SUBROUTINE QRHOUSE_HESS(DIM,MATRIX,Q,R,CHECK)
+    ! --- FOR ANY REPORT OR SUGGESTION, PLEASE CONTACT quentin.marecat@etu.umontpellier.fr --- !
+    ! -------------------------------------------------------------------------- !
+    ! --- THIS SUBROUTINE GENERATE QR DECOMPOSITION USING HOUSEHOLDER SCHEME --- !
+    ! --- MATRIX MUST BE HESSENBERG MATRIX ------------------------------------- !
+    ! --- Q IS AN HESSENBERG MATRIX -------------------------------------------- !
+    ! --- HOUSEMAT AND MAT CAN BE THE SAME BUT MAT WILL BE ERASED -------------- !
+    ! --- CHECK = .TRUE. IF VERIFICATIONS HAVE TO BE DONE ---------------------- !
+    ! -------------------------------------------------------------------------- !
+    IMPLICIT NONE
+    LOGICAL, INTENT(IN) :: CHECK
+    REAL*8, PARAMETER :: EPS0 = 1.D-15, CONV = 1.D-8
+    LOGICAL :: TEST
+    INTEGER, INTENT(IN) :: DIM
+    REAL*8, INTENT(IN) :: MATRIX(DIM,DIM)
+    REAL*8, INTENT(OUT) :: R(DIM,DIM),Q(DIM,DIM)
+    REAL*8 :: VEC(DIM),MATROTTAMP(DIM,DIM), PROD(DIM,DIM)
+    REAL*8 :: MATROTT(DIM,DIM),ID(DIM,DIM),MATTAMP(DIM,DIM)
+    REAL*8 :: MATROTM1(DIM,DIM)
+    INTEGER :: I,J,K,LOWESTELEM,COMPT,ERR, STAT
+    ERR = 97
+    OPEN(UNIT = ERR, FILE = 'error', IOSTAT = STAT, STATUS = 'old')
+    IF (STAT == 0) CLOSE(ERR,STATUS = 'delete')
+    
+    R = MATRIX 
+    ID = 0.
+    DO I = 1,DIM
+        ID(I,I) = 1.
+    ENDDO
+    Q = ID
+    MATROTM1 = ID
+    DO LOWESTELEM = 1,DIM-1
+        VEC = 0
+        DO I = LOWESTELEM,LOWESTELEM+1
+            VEC(I) = R(I,LOWESTELEM)
+        ENDDO 
+        CALL HOUSESTEP_TRI(DIM,VEC,LOWESTELEM,MATROTTAMP)
+        ! --- MATROTTAMP IS ID MATRIX WHERE ONLY 2*2 BLOC ELEMENT ON LOWESTELEM ARE MODIFIED --- !
+!        CALL PRODMAT(DIM,MATROTTAMP,R,R)
+        CALL PRODMAT_BLOC_QR_TRI(DIM,LOWESTELEM-1,MATROTTAMP,R,R) ! --- IMPROVEMENT OF PRODMAT --- !
+!        CALL PRODMAT(DIM,Q,MATROTTAMP,Q)
+        ! --- Q IS ID MATRIX WHERE ONLY 2*2 BLOC ELEMENT ON LOWESTELEM ARE MODIFIED --- !
+        CALL PRODMAT_BLOC_Q_TRI(DIM,LOWESTELEM-1,Q,MATROTTAMP,Q) ! --- IMPROVEMENT OF PRODMAT --- !
+    ENDDO
+
+        ! --- VERIFICATION --- !
+    IF (CHECK) THEN
+        ERR = 97
+        TEST = .FALSE.
+        DO I = 1,DIM-1
+            DO J = I+1,DIM
+                IF (ABS(R(J,I)) > CONV) TEST = .TRUE.
+            ENDDO
+        ENDDO
+
+        CALL PRODMAT(DIM,Q,R,MATTAMP)
+        DO I = 1,DIM
+            DO J = 1,DIM
+                IF (ABS(MATTAMP(J,I) - MATRIX(J,I)) > CONV) TEST = .TRUE.
+            ENDDO
+        ENDDO
+
+        IF (TEST) THEN
+            OPEN(UNIT = ERR,FILE = 'error')
+            WRITE(ERR,'(A)') 'ERROR QR DECOMPOSITION HOUSEHOLDER'
+            WRITE(ERR,'(A,4X,ES14.5)') 'CRIT CONV',CONV
+            WRITE(ERR,'(A)') 'Q ='
+            DO I = 1,DIM
+                WRITE(ERR,'(100F14.5)') (Q(I,J), J = 1,DIM)
+            ENDDO 
+            WRITE(ERR,'(A)') '*************'
+            WRITE(ERR,'(A)') 'R ='
+            DO I = 1,DIM
+                WRITE(ERR,'(100F14.5)') (R(I,J), J = 1,DIM)
+            ENDDO 
+            WRITE(ERR,'(A)') '*************'
+            WRITE(ERR,'(A)') 'QR - M ='
+            DO I = 1,DIM
+                WRITE(ERR,'(100ES14.5)') ((MATTAMP(J,I) - MATRIX(J,I)), J = 1,DIM)
+            ENDDO 
+        WRITE(ERR,'(A)') '*************'
+        ENDIF
+    ENDIF
+
+    OPEN(UNIT = ERR, FILE = 'error', IOSTAT = STAT, STATUS = 'old')
+    IF (STAT == 0) WRITE(6,'(A)') 'ERROR QR HOUSEHOLDER, SEE FILE error'
+END SUBROUTINE
---
+++ QR_dec/test_hess.f90
@@ -0,0 +1,50 @@
+PROGRAM TEST
+    IMPLICIT NONE
+    REAL*8,ALLOCATABLE :: M(:,:), Q(:,:), R(:,:)
+    INTEGER :: DIM,I,J, STAT
+    OPEN(UNIT = 99,FILE = 'input',STATUS = 'OLD', ACTION= 'READ')
+    OPEN(UNIT = 98,FILE = 'output')
+!    READ(99,*) DIM
+!    ALLOCATE(M(DIM,DIM),Q(DIM,DIM),R(DIM,DIM))
+!    DO I = 1,DIM
+!        READ(99,*) (M(I,J), J = 1,DIM)
+!   ENDDO
+! --- M IS HESSENBERG MATRIX --- !
+    DIM = 100
+    ALLOCATE(M(DIM,DIM),Q(DIM,DIM),R(DIM,DIM))
+    M = 0
+    CALL RANDOM_NUMBER(M)
+    DO I = 1,DIM
+        DO J = I,DIM
+        M(I,J) = 200*M(I,J) - 100
+        M(J,I) = M(I,J)
+        ENDDO
+    ENDDO
+    DO I = 3,DIM
+        DO J = 1,I-2
+            M(I,J) = 0
+        ENDDO
+    ENDDO
+
+    CALL QRHOUSE_HESS(DIM,M,Q,R,.TRUE.)
+
+    WRITE(98,*) 'QR DEC'
+    WRITE(98,'(A)') 'INITIAL MATRIX'
+    DO I = 1,DIM
+        WRITE(98,'(100F14.5)') (M(I,J),J = 1,DIM)
+    ENDDO
+    WRITE(98,*) '*******************'
+    WRITE(98,'(A)') 'Q UNITARY MATRIX'
+    DO I = 1,DIM
+        WRITE(98,'(100F14.5)') (Q(I,J),J = 1,DIM)
+    ENDDO
+    WRITE(98,*) '*******************'
+    WRITE(98,'(A)') 'R UPPER MATRIX'
+    DO I = 1,DIM
+        WRITE(98,'(100F14.5)') (R(I,J),J = 1,DIM)
+    ENDDO
+    WRITE(98,*) '*******************'
+
+    OPEN(UNIT = 97, FILE = 'error', IOSTAT = STAT, STATUS = 'old')
+    IF (STAT == 2) WRITE(6,'(A)') 'RUN SUCCESFULLY, SEE output FILE' 
+END
