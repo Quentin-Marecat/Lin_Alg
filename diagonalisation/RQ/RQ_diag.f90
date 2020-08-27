9338c3e243f6cc440234db92fe0d57c173ada41e
Quentin <quentin.marecat@gmail.com>
2020-08-27 10:54:57 +0200

    RQ diag mod

--- diagonalisation/RQ/RQ_diag.f90
+++ diagonalisation/RQ/RQ_diag.f90
@@ -10,17 +10,16 @@ SUBROUTINE RQ_DIAG(DIM,MAT,EIGENVAL,EIGENVEC,SYM,EGNVEC,CHECK)
     ! --- IF CHECK = .TRUE., EGNVEC = .TRUE. ------------------------------------------ !
     ! --------------------------------------------------------------------------------- !
     IMPLICIT NONE
-    REAL*8, PARAMETER :: CONV = 1.D-12, EPS0 = 1.D-15
+    REAL*8, PARAMETER :: EPS0 = 1.D-15, CONV = 1.D-12
     INTEGER, PARAMETER :: MAXSTEP = 1D4
-    LOGICAL,INTENT(IN) :: CHECK, EGNVEC
     LOGICAL :: TEST
-    LOGICAL,INTENT(IN) :: SYM
+    LOGICAL,INTENT(IN) :: CHECK, EGNVEC, SYM
     INTEGER, INTENT(IN) :: DIM
     REAL*8, INTENT(IN) :: MAT(DIM,DIM)
     REAL*8, INTENT(OUT) :: EIGENVEC(DIM,DIM), EIGENVAL(DIM)
     REAL*8 :: TRISUP(DIM,DIM),MATROT(DIM,DIM),MATTRI(DIM,DIM)
     REAL*8 :: ROTTRISUP(DIM,DIM), ID(DIM,DIM),MATTAMP(DIM,DIM)
-    REAL*8 :: RES,RES2, VEC(DIM),NOR(DIM)
+    REAL*8 :: RES,RES2, VEC(DIM),NOR(DIM), EIGENVECT(DIM,DIM)
     INTEGER COMPT, STAT, ERR, I, J, K, VALSHIFT
 
     TEST = .FALSE.
@@ -35,30 +34,16 @@ SUBROUTINE RQ_DIAG(DIM,MAT,EIGENVAL,EIGENVEC,SYM,EGNVEC,CHECK)
     ! --- STEP 1 : TRIDIAGONAL DECOMPO OF THE MATRIX --- !
     MATROT = ID
     MATTRI = MAT
-    IF (SYM) CALL TRIDIAG_QR(DIM,MAT,MATTRI,MATROT,.TRUE.)
+    IF (SYM) CALL TRIDIAG_HOUSE(DIM,MAT,MATTRI,MATROT,.TRUE.)
     MATTAMP = MATTRI
     ! --- STEP 2 : RQ ITERATION WITH SHIFT UNTIL CONVERGENCE --- !
     EIGENVEC = ID
-    VALSHIFT = DIM + 1
     RES = 1 
     COMPT = 0
     DO WHILE(RES > CONV .AND. COMPT < MAXSTEP) 
         CALL QRHOUSE_TRI(DIM,MATTRI,ROTTRISUP,TRISUP,.FALSE.)
         CALL PRODMAT_RQ(DIM,TRISUP,ROTTRISUP,MATTRI)
         IF (EGNVEC) CALL PRODMAT(DIM,EIGENVEC,ROTTRISUP,EIGENVEC)
-!        ! --- 0 ELEMENT CALCULATION --- !
-!        RES = 0
-!        DO I = 1,DIM
-!            DO J = 1,DIM
-!                VEC(J) = EIGENVEC(J,I)
-!            ENDDO
-!            CALL MATAPPLI_TRI(DIM,MATTAMP,VEC,VEC)
-!            DO J = 1,DIM
-!                VEC(J) = VEC(J) - MATTRI(I,I)*EIGENVEC(J,I)
-!            ENDDO
-!            CALL NORMVEC(DIM,VEC,RES2)
-!            IF (RES2 > RES) RES = RES2
-!        ENDDO
         ! --- 0 ELEMENT CALCULATION --- !
         RES = 0
         DO I = 2,DIM
@@ -67,6 +52,7 @@ SUBROUTINE RQ_DIAG(DIM,MAT,EIGENVAL,EIGENVEC,SYM,EGNVEC,CHECK)
             ENDDO
         ENDDO
         RES = SQRT(2*RES)
+        ! ----------------------------- !
         COMPT = COMPT + 1
     ENDDO
     DO I = 1,DIM
@@ -81,13 +67,6 @@ SUBROUTINE RQ_DIAG(DIM,MAT,EIGENVAL,EIGENVEC,SYM,EGNVEC,CHECK)
     IF (CHECK) THEN
         TEST = .FALSE.
         IF (COMPT == MAXSTEP) TEST = .TRUE.
-        CALL PRODMAT(DIM,MAT,EIGENVEC,MATTAMP)
-        DO I = 1,DIM
-            DO J = 1,DIM
-                VEC(J) = MATTAMP(J,I) - EIGENVAL(I)*EIGENVEC(J,I)
-            ENDDO
-            CALL NORMVEC(DIM,VEC,NOR(I))
-        ENDDO
         IF (TEST) THEN
             OPEN(UNIT = ERR, FILE = 'error')
             WRITE(ERR,'(A,10X,A,4X,I14.5,10X,A,4X,I14.5)') 'PROBLEM DIAGONALISATION'&
@@ -97,9 +76,28 @@ SUBROUTINE RQ_DIAG(DIM,MAT,EIGENVAL,EIGENVEC,SYM,EGNVEC,CHECK)
             WRITE(ERR,'(A)') '***********************'
             WRITE(ERR,'(A)') 'DIAGONAL MATRIX OBTAINED'
             DO I = 1,DIM
-                WRITE(ERR,'(100ES14.5)') (MATTRI(I,J),J=1,DIM)
+                WRITE(ERR,'(100F14.5)') (MATTRI(I,J),J=1,DIM)
             ENDDO
             WRITE(ERR,'(A)') '***********************'
+            MATTRI = ID
+            DO I = 1,DIM
+                MATTRI(I,I) = EIGENVAL(I)
+            ENDDO
+            CALL PRODMAT(DIM,EIGENVEC,MATTRI,MATTAMP)
+            CALL TRANSPOSE(DIM,EIGENVEC,EIGENVECT)
+            CALL PRODMAT(DIM,MATTAMP,EIGENVECT,MATTAMP)
+            WRITE(ERR,'(A)') ' Q*D*Q(T) - MAT'
+            DO I = 1,DIM
+                WRITE(ERR,'(100ES14.5)') (MATTAMP(I,J)-MAT(I,J), J=1,DIM)
+            ENDDO
+            WRITE(ERR,'(A)') '***********************'
+            CALL PRODMAT(DIM,MAT,EIGENVEC,MATTAMP)
+            DO I = 1,DIM
+                DO J = 1,DIM
+                    VEC(J) = MATTAMP(J,I) - EIGENVAL(I)*EIGENVEC(J,I)
+                ENDDO
+                CALL NORMVEC(DIM,VEC,NOR(I))
+            ENDDO
             WRITE(ERR,'(A)') 'MAT*EIGENVEC - EIGENVAL*EIGENVEC = 0'
             DO I = 1,DIM
                 WRITE(ERR,'(100ES14.5)')(MATTAMP(J,I) - EIGENVAL(I)*EIGENVEC(J,I),J=1,DIM)
--- diagonalisation/RQ/RQ_shift_diag.f90
+++ diagonalisation/RQ/RQ_shift_diag.f90
@@ -5,23 +5,21 @@ SUBROUTINE RQ_SHIFT_DIAG(DIM,MAT,EIGENVAL,EIGENVEC,SYM,EGNVEC,CHECK)
     ! --- USING RQ ITERATIVE ALGORITHME WITH SHIFTING IMPROVEMENT --------------------- !
     ! --- NECESSARY IF IT INVOLVE SOME DEGENERANCIES WITH EIGENVALUES ----------------- !
     ! --- SYM = .TRUE. IF THE MATRIX IS SYMMETRIC ------------------------------------- !
-    ! --- operation_mat.f90 tridiag.f90 QRhouse.f90 and housestep.f90 ARE NECESSARY --- !
     ! --- CHECK = .TRUE. IF VERIFICATIONS HAS TO BE DONE ------------------------------ !
     ! --- EGNVEC = .TRUE. IF EIGENVECTORS MUST BE COMPUTE ----------------------------- !
     ! --- IF CHECK = .TRUE., EGNVEC = .TRUE. ------------------------------------------ !
     ! --------------------------------------------------------------------------------- !
     IMPLICIT NONE
-    REAL*8, PARAMETER :: CONV = 1.D-12, EPS0 = 1.D-15
+    REAL*8, PARAMETER :: EPS0 = 1.D-15, CONV = 1.D-12
     INTEGER, PARAMETER :: MAXSTEP = 1D4
-    LOGICAL,INTENT(IN) :: CHECK, EGNVEC
     LOGICAL :: TEST
-    LOGICAL,INTENT(IN) :: SYM
+    LOGICAL,INTENT(IN) :: CHECK, EGNVEC, SYM
     INTEGER, INTENT(IN) :: DIM
     REAL*8, INTENT(IN) :: MAT(DIM,DIM)
     REAL*8, INTENT(OUT) :: EIGENVEC(DIM,DIM), EIGENVAL(DIM)
     REAL*8 :: TRISUP(DIM,DIM),MATROT(DIM,DIM),MATTRI(DIM,DIM)
     REAL*8 :: ROTTRISUP(DIM,DIM), ID(DIM,DIM),MATTAMP(DIM,DIM)
-    REAL*8 :: RES,RES2, VEC(DIM)
+    REAL*8 :: RES,RES2, VEC(DIM), EIGENVECT(DIM,DIM)
     REAL*8 ::  SHIFT, NOR(DIM)
     INTEGER COMPT, STAT, ERR, I, J, K, VALSHIFT
 
@@ -37,7 +35,7 @@ SUBROUTINE RQ_SHIFT_DIAG(DIM,MAT,EIGENVAL,EIGENVEC,SYM,EGNVEC,CHECK)
     ! --- STEP 1 : TRIDIAGONAL DECOMPO OF THE MATRIX --- !
     MATROT = ID
     MATTRI = MAT
-    IF (SYM) CALL TRIDIAG_QR(DIM,MAT,MATTRI,MATROT,.TRUE.)
+    IF (SYM) CALL TRIDIAG_HOUSE(DIM,MAT,MATTRI,MATROT,.TRUE.)
     MATTAMP = MATTRI
     ! --- STEP 2 : RQ ITERATION WITH SHIFT UNTIL CONVERGENCE --- !
     EIGENVEC = ID
@@ -56,19 +54,6 @@ SUBROUTINE RQ_SHIFT_DIAG(DIM,MAT,EIGENVAL,EIGENVEC,SYM,EGNVEC,CHECK)
             MATTRI(I,I) = MATTRI(I,I) + SHIFT
         ENDDO 
         IF (EGNVEC) CALL PRODMAT(DIM,EIGENVEC,ROTTRISUP,EIGENVEC)
-!        ! --- 0 ELEMENT CALCULATION --- !
-!        RES = 0
-!        DO I = 1,DIM
-!            DO J = 1,DIM
-!                VEC(J) = EIGENVEC(J,I)
-!            ENDDO
-!            CALL MATAPPLI_TRI(DIM,MATTAMP,VEC,VEC)
-!            DO J = 1,DIM
-!                VEC(J) = VEC(J) - MATTRI(I,I)*EIGENVEC(J,I)
-!            ENDDO
-!            CALL NORMVEC(DIM,VEC,RES2)
-!            IF (RES2 > RES) RES = RES2
-!        ENDDO
         ! --- 0 ELEMENT CALCULATION --- !
         RES = 0
         DO I = 2,DIM
@@ -77,6 +62,7 @@ SUBROUTINE RQ_SHIFT_DIAG(DIM,MAT,EIGENVAL,EIGENVEC,SYM,EGNVEC,CHECK)
             ENDDO
         ENDDO
         RES = SQRT(2*RES)
+        ! ----------------------------- !
         ! --- SHIFT PROCESS --- !
         VALSHIFT = VALSHIFT - 1
         IF (VALSHIFT < 1) VALSHIFT = DIM
@@ -111,9 +97,28 @@ SUBROUTINE RQ_SHIFT_DIAG(DIM,MAT,EIGENVAL,EIGENVEC,SYM,EGNVEC,CHECK)
             WRITE(ERR,'(A)') '***********************'
             WRITE(ERR,'(A)') 'DIAGONAL MATRIX OBTAINED'
             DO I = 1,DIM
-                WRITE(ERR,'(100ES14.5)') (MATTRI(I,J),J=1,DIM)
+                WRITE(ERR,'(100F14.5)') (MATTRI(I,J),J=1,DIM)
             ENDDO
             WRITE(ERR,'(A)') '***********************'
+            MATTRI = ID
+            DO I = 1,DIM
+                MATTRI(I,I) = EIGENVAL(I)
+            ENDDO
+            CALL PRODMAT(DIM,EIGENVEC,MATTRI,MATTAMP)
+            CALL TRANSPOSE(DIM,EIGENVEC,EIGENVECT)
+            CALL PRODMAT(DIM,MATTAMP,EIGENVECT,MATTAMP)
+            WRITE(ERR,'(A)') ' Q*D*Q(T) - MAT'
+            DO I = 1,DIM
+                WRITE(ERR,'(100ES14.5)') (MATTAMP(I,J)-MAT(I,J), J=1,DIM)
+            ENDDO
+            WRITE(ERR,'(A)') '***********************'
+            CALL PRODMAT(DIM,MAT,EIGENVEC,MATTAMP)
+            DO I = 1,DIM
+                DO J = 1,DIM
+                    VEC(J) = MATTAMP(J,I) - EIGENVAL(I)*EIGENVEC(J,I)
+                ENDDO
+                CALL NORMVEC(DIM,VEC,NOR(I))
+            ENDDO
             WRITE(ERR,'(A)') 'MAT*EIGENVEC - EIGENVAL*EIGENVEC = 0'
             DO I = 1,DIM
                 WRITE(ERR,'(100ES14.5)')(MATTAMP(J,I) - EIGENVAL(I)*EIGENVEC(J,I),J=1,DIM)
